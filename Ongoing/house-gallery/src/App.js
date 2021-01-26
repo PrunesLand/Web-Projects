2963755745d9fb4ab1eb05370c9f482f8df7ea02
Pranaya Anargya <panargya@gmail.com>
2021-01-26 14:45:38 +1100

    gallery first commit

---
+++ Ongoing/house-gallery/src/App.js
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gallery Project</h1>
+      <p>Ongoing project developed by Pran</p>
+    </div>
+  );
+}
+
+export default App;
