bc0221a717bc8d0e6d979ec899cc4c80aabfd37e
Pranaya Anargya <panargya@gmail.com>
2021-01-31 12:19:00 +1100

    started parralax

---
+++ Ongoing/house-gallery/src/Components/parallax-section/Parallax.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {HeroContainer, HeroSection} from './ParallaxStyles'
+
+const Parallax = () => {
+    return (
+        <HeroSection>
+            <HeroContainer>
+                <h1>Parralax</h1>
+            </HeroContainer>
+        </HeroSection>
+    )
+}
+
+export default Parallax
---
+++ Ongoing/house-gallery/src/Components/parallax-section/ParallaxStyles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const HeroSection = styled.section`
+    height:100vh;
+`
+
+export const HeroContainer = styled.div``
