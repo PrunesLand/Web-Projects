975cb7cd56a5f4318d2687531b40e6e33cfdc7c7
Pranaya Anargya <panargya@gmail.com>
2021-02-01 17:08:41 +1100

    added intro to yard

--- Ongoing/house-gallery/src/Components/parallax-section/Parallax.js
+++ Ongoing/house-gallery/src/Components/parallax-section/Parallax.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import {HeroContainer, HeroSection} from './ParallaxStyles'
+import {HeroContainer, HeroSection, HeroWrapper, HeroImageWrapper, HeroImage,  HeroText, TextContainer, HeroHeader} from './ParallaxStyles'
+
 
 const Parallax = () => {
     return (
         <HeroSection>
             <HeroContainer>
-                <h1>Parralax</h1>
+                <HeroWrapper>
+                    <HeroImageWrapper>
+                        <HeroHeader>
+                            Yard
+                        </HeroHeader>
+                        
+                        
+                    </HeroImageWrapper>
+                </HeroWrapper>
+                <HeroText>
+                    <TextContainer>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum mollitia impedit blanditiis voluptate soluta. Sint natus non quo praesentium, cupiditate quibusdam nam itaque tempore sequi quas nihil, voluptatibus asperiores blanditiis.
+                    </TextContainer>
+                </HeroText>
             </HeroContainer>
         </HeroSection>
     )
--- Ongoing/house-gallery/src/Components/parallax-section/ParallaxStyles.js
+++ Ongoing/house-gallery/src/Components/parallax-section/ParallaxStyles.js
@@ -1,7 +1,54 @@
 import styled from 'styled-components'
+import statue3 from '../Images/Finalized images/statue3.JPG'
 
 export const HeroSection = styled.section`
-    height:100vh;
+    height:100vw;
+    max-height:100vh;
+    overflow:hidden;
 `
 
 export const HeroContainer = styled.div``
+
+export const HeroWrapper = styled.div`
+    
+    height:100%;
+    padding:2vh 0;
+`
+
+
+export const HeroImageWrapper = styled.div`
+    background-image: url(${statue3});
+    width:100%;
+    height:50vw;
+    max-height:60vh;
+    background-size:cover;
+    background-position:center;
+    
+`
+export const HeroHeader = styled.div`
+    position:absolute;
+    width:100%;
+    max-height:80vh;
+    font-size:10vw;
+    color:white;
+   
+
+`
+
+export const HeroImage = styled.img`
+    
+    width:100%;
+    max-height:80vh;
+    object-fit:contain;
+    
+`
+
+export const HeroText = styled.div`
+    width:100%;
+    padding:0 10vw;
+    height:30vh;
+`
+
+export const TextContainer = styled.div`
+    
+`
