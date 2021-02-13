b30d0896287e6191e74b799507353ce3e72f8475
Pranaya Anargya <panargya@gmail.com>
2021-02-13 15:18:35 +1100

    merge push

--- References/slider-app/src/App.js
+++ References/slider-app/src/App.js
@@ -21,11 +21,12 @@ function App() {
     }
   ]
   const [counter, setCounter] = useState(0)
-  
+  const [catcher, setCatcher] = useState(photos[0].photo)
 
   
   const add = () => {
     resetCount()
+    changeImg()
     setCounter(counter + 1)
     console.log(counter)
   }
@@ -33,14 +34,28 @@ function App() {
  
 
   const subt = () => {
-    setCounter(counter - 1)
-    resetCount()
+   setCounter(counter - 1)
+    
+  }
+
+  const changeImg = () => {
+   switch (counter){
+      case 0:
+        setCatcher(photos[1].photo)
+        break;
+      case 1:
+        setCatcher(photos[2].photo)
+        break;
+      case 3:
+        
+        setCatcher(photos[0].photo)
+        break;
+    }
   }
 
   const resetCount = () => {
-    
-    
-    if(counter > 1){
+    if(counter > 3){
+
       setCounter(0)
     }
   }
@@ -53,7 +68,7 @@ function App() {
       <h2>{counter}</h2>
       {/* <button onClick={subt}>subtract</button> */}
       <button onClick={add}>change</button>
-      <img src={photos[counter].photo} alt='test' style={{height: '200px', objectFit: "cover"}}/>
+      <img src={catcher} alt='test' style={{height: '200px', objectFit: "cover"}}/>
     </section>
   );
 }
