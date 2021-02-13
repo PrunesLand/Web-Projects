be57ce972e64540c12b6e86bdef7f153cf9103d0
Pranaya Anargya <panargya@gmail.com>
2021-02-13 11:12:53 +1100

    test push

--- References/slider-app/src/App.js
+++ References/slider-app/src/App.js
@@ -1,28 +1,59 @@
-import React,{ useState } from "react";
-import {SkillData} from "./SkillData";
+/* eslint-disable default-case */
+import React,{ useState, useEffect } from "react";
+import test1 from './Images/GridImages/css.jpg'
+import test2 from './Images/GridImages/html.jpg'
+import test3 from './Images/GridImages/java.jpg'
 
 function App() {
-  const [skill, setSkill] = useState(SkillData);
-  const [index,setIndex] = useState(0);
+  
+  const photos = [
+    {
+      id:0,
+      photo:test1
+    },
+    {
+      id:1,
+      photo:test2
+    },
+    {
+      id:2,
+      photo: test3
+    }
+  ]
+  const [counter, setCounter] = useState(0)
+  
+
+  
+  const add = () => {
+    resetCount()
+    setCounter(counter + 1)
+    console.log(counter)
+  }
+
+ 
+
+  const subt = () => {
+    setCounter(counter - 1)
+    resetCount()
+  }
+
+  const resetCount = () => {
+    
+    
+    if(counter > 1){
+      setCounter(0)
+    }
+  }
+  
+
+  
 
   return (
-    <section className='section'>
-      <div className='title'>
-        <h2>
-          <span>/</span> skills
-        </h2>
-      </div>
-      <div className='section-center'>
-        {skill.map((item, itemIndex ) => {
-          const {id, image , title, text} = item;
-
-          return (
-            <article key={id}>
-              <img src={image} alt={title} className='person-img' />
-            </article>
-          )
-        })}
-      </div>
+    <section >
+      <h2>{counter}</h2>
+      {/* <button onClick={subt}>subtract</button> */}
+      <button onClick={add}>change</button>
+      <img src={photos[counter].photo} alt='test' style={{height: '200px', objectFit: "cover"}}/>
     </section>
   );
 }
